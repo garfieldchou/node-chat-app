4aed60a445a2ac9da043d6fe790aa9c78115a84a
garfield <cmgarfield@gmail.com>
2017-12-04 20:06:19 +0800

    get back a web socket server which can emit or listen to events

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
