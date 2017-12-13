a277875fec94e517464801a22f0455aeb6587e4e
garfield <cmgarfield@gmail.com>
2017-12-13 23:24:19 +0800

    use text() method to change the text of the botton while sending location

--- public/js/index.js
+++ public/js/index.js
@@ -45,16 +45,16 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by your browser.');
   }
 
-  locationButton.attr('disabled', 'disabled');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
-    locationButton.removeAttr('disabled');
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
-    locationButton.removeAttr('disabled');
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
