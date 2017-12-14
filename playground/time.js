03b50ff382a46421210d9adae4c69c5f05e259b9
garfield <cmgarfield@gmail.com>
2017-12-14 22:53:40 +0800

    use token in format() to get formatted string for date
    refer to
    http://momentjs.com/docs/#/displaying/

--- playground/time.js
+++ playground/time.js
@@ -1,4 +1,8 @@
+var moment = require('moment');
 // Jan 1st 1970 00:00:00 am
 
-var date = new Date();
-console.log(date.getMonth());  
+// var date = new Date();
+// console.log(date.getMonth());
+
+var date = moment();
+console.log(date.format('MMM YYYY'));
