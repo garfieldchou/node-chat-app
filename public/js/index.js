9c4bf1802487750e58dc4523d993ecd9fd17fbc6
garfield <cmgarfield@gmail.com>
2017-12-15 21:12:54 +0800

    add formatted timestamp to new location messages

--- public/js/index.js
+++ public/js/index.js
@@ -17,10 +17,11 @@ socket.on('newMessage', function(message) {
 });
 
 socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
