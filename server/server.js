c8a2144d2ba9e7afbd337b5da20ed7c2dccf4d71
garfield <cmgarfield@gmail.com>
2017-12-17 15:03:06 +0800

    remove the user from list when the user disconnect and then update the user list, show an admin message

--- server/server.js
+++ server/server.js
@@ -45,7 +45,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
   });
 });
 
