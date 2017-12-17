07f0ccc63cc49692977b34ca77c5a0c1b56881af
garfield <cmgarfield@gmail.com>
2017-12-17 11:37:50 +0800

    add getUserList() method with filter() & map() and its corresponding test case

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,6 +7,18 @@ class Users {
      this.users.push(user);
      return user;
   }
+  removeUser (id) {
+
+  }
+  getUser (id) {
+
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,24 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Mike',
+      room: 'Node Course'
+    }, {
+      id: '2',
+      name: 'Jen',
+      room: 'React Course'
+    }, {
+      id: '3',
+      name: 'Julie',
+      room: 'Node Course'
+    }];
+  });
 
   it('should add new user', () => {
     var users = new Users();
@@ -16,4 +34,10 @@ describe('Users', () => {
     expect(users.users).toEqual([user]);
   });
 
- });
+  it('should return names for Node course', () => {
+    var userList = users.getUserList('Node Course');
+
+    expect(userList).toEqual(['Mike', 'Julie']);
+  });
+
+});
