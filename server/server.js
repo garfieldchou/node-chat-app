da8444781249864d0ae9f3ad5283902695cbfb34
garfield <cmgarfield@gmail.com>
2017-12-17 20:09:48 +0800

    emit newMessage only to the users in the same room

--- server/server.js
+++ server/server.js
@@ -20,8 +20,12 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback();
   });
 
