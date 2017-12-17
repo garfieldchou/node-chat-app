b33f4944b8518c5334b119a4e224c2f7b27addb4
garfield <cmgarfield@gmail.com>
2017-12-17 13:15:00 +0800

    implement removeUser(id) method and its corresponding test cases

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -35,11 +35,19 @@ describe('Users', () => {
   });
 
   it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
 
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
   });
 
   it('should not remove  user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
 
+    expect(user).toNotExist();
+    expect(users.users.length).toBe(3);
   });
 
   it('should find user', () => {
